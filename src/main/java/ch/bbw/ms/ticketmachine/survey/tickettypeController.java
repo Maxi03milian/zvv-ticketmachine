91586caea376c107dd5bbf9e47fc504d01ee92fa
Maxi <46729527+Maxi03milian@users.noreply.github.com>
2022-07-07 11:32:46 +0200

    button back to main page

--- src/main/java/ch/bbw/ms/ticketmachine/survey/tickettypeController.java
+++ src/main/java/ch/bbw/ms/ticketmachine/survey/tickettypeController.java
@@ -2,12 +2,17 @@ package ch.bbw.ms.ticketmachine.survey;
 
 import ch.bbw.ms.ticketmachine.survey.model.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class tickettypeController {
 
     @Autowired
@@ -16,6 +21,13 @@ public class tickettypeController {
     @GetMapping("/tickettypeform")
     public String tickettypeform(Model model){
         model.addAttribute("ticket", myTicket);
-        return "tickettypeform.html";
+        return "tickettypeform";
+    }
+
+    @PostMapping("/tickettypeform")
+    public String saveActivity(@ModelAttribute Ticket ticket) {
+        myTicket = ticket;
+        System.out.println(ticket);
+        return "redirect:/index.html";
     }
 }
