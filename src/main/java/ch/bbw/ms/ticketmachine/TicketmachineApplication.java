b13726073e172567af02f9050c661d844ffeb6d1
Maxi <46729527+Maxi03milian@users.noreply.github.com>
2022-07-07 09:41:17 +0200

    added base spring boot project

---
+++ src/main/java/ch/bbw/ms/ticketmachine/TicketmachineApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.ms.ticketmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/ms/ticketmachine/TicketmachineApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.ms.ticketmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
